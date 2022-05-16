84a000f11c5d52fe83c13f8891a227c801292d08
Vova2plova <vova2plova@DESKTOP-ELL9JA1.localdomain>
2022-05-16 22:54:09 +0300

    321

--- Styles/Styles/obj/Debug/net6.0-windows/Controls/DoublePathButton.g.i.cs
+++ Styles/Styles/obj/Debug/net6.0-windows/Controls/DoublePathButton.g.i.cs
@@ -63,13 +63,13 @@ namespace UI.Controls {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.5.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Styles;V1.0.0.0;component/controls/doublepathbutton.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Styles;component/controls/doublepathbutton.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Controls\DoublePathButton.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -79,7 +79,7 @@ namespace UI.Controls {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.5.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
